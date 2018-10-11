ba982b2f81872d6a1f998d1dfecb1a5ac44e7686
VladislavOvsyannikov <VladislavOvsyannikov@users.noreply.github.com>
2018-10-11 22:55:05 +0300

    user

--- src/main/java/system/model/User.java
+++ src/main/java/system/model/User.java
@@ -1,11 +1,10 @@
 package system.model;
 
 import javax.persistence.*;
-import java.io.Serializable;
 
 @Entity
 @Table(name = "user", schema = "fias")
-public class User implements Serializable {
+public class User{
 
     @Id
     @GeneratedValue(strategy = GenerationType.SEQUENCE)
